76175d0ecbf2a12799f978db506458db82038fa2
nevikw39 <nevikw39@icloud.com>
2022-11-16 21:24:53 +0800

    sshpass with TOTP support

---
+++ Formula/sshpass_otp.rb
@@ -0,0 +1,21 @@
+class Sshpass_pass < Formula
+  desc "sshpass with TOTP support"
+  homepage "https://github.com/dora38/sshpass"
+  url "https://github.com/dora38/sshpass/archive/refs/heads/main.zip"
+  version "1.09.1"
+  sha256 ""
+  license ""
+
+  depends_on "automake"
+
+
+  def install
+    system "./bootstrap"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
