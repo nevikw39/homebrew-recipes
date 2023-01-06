b3f926e27c5d1c01fb86460c416b734e6b5f8d9b
nevikw39 <nevikw39@icloud.com>
2023-01-06 16:51:39 +0800

    FTDI USB Serial Dext Installer

---
+++ Casks/ftdi-usb-serial-dext-installer.rb
@@ -0,0 +1,11 @@
+cask "ftdi-usb-serial-dext-installer" do
+    version "1.5.0"
+    sha256 :no_check
+
+    url "https://ftdichip.com/wp-content/uploads/2022/06/FTDIUSBSerialDextInstaller_#{version.dots_to_underscores}.dmg"
+    name "FTDI USB Serial Dext Installer"
+    desc ""
+    homepage "https://ftdichip.com/drivers/vcp-drivers/"
+
+    app "FTDIUSBSerialDextInstaller_1_5_0.app"
+end
