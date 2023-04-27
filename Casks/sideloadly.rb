6bdafff772479f11de231f4049ed0a3c68e52df0
nevikw39 <nevikw39@icloud.com>
2023-04-27 20:02:14 +0800

    Sideloadly

---
+++ Casks/sideloadly.rb
@@ -0,0 +1,11 @@
+cask "sideloadly" do
+    version "0.40.5"
+    sha256 :no_check
+
+    url "https://sideloadly.io/SideloadlySetup.dmg"
+    name "Sideloadly"
+    desc ""
+    homepage "https://sideloadly.io"
+
+    app "Sideloadly.app"
+end
